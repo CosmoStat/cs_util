ee4907192ef7d626a9fa3a79fbcea11b6a0eab47
Martin Kilbinger <martin.kilbinger@cea.fr>
2022-10-12 20:33:16 +0200

    missing lib for API doc

--- cs_util/tests/test_plots.py
+++ cs_util/tests/test_plots.py
@@ -2,52 +2,91 @@
 
 """UNIT TESTS FOR PLOTS SUBPACKAGE.
 
-This module contains unit tests for the plotsw subpackage.
+This module contains unit tests for the plots subpackage.
 
 """
 
 import os
 
 import numpy as np
+from matplotlib.figure import Figure
 from numpy import testing as npt
 
 from unittest import TestCase
 
-from cs_util import calc, plots
+from cs_util import plots
 
 
-class CalcTestCase(TestCase):
-    """Test case for the ``calc`` module."""
+class PlotsTestCase(TestCase):
+    """Test case for the ``plots`` module."""
 
     def setUp(self):
-        """Set test parameter values."""
-        self._x = np.array([1.0, 2.0, 1.5, 0.7])
-        self._w = np.array([0.8, 1.2, 2.0, 1.5])
-        self._mean_w = 725 / 550
-        self._std_w = 0.4820754030386492
-        self._std_w_corr = 0.5566527274281229
-
-
-    def tearDown(self):
-        """Unset test parameter values."""
-        self._x = None
-        self._w = None
-        self._mean_w = None
-        self._std_w = None
-        self._std_w_corr = None
-
-    def test_weighted_avg_and_std(self):
-        """Test ``plots.weighted_avg_and_std`` method.
+        """Set test parameter values.
 
         """
-        mean_w, std_w = calc.weighted_avg_and_std(self._x, self._w)
-        npt.assert_almost_equal(mean_w, self._mean_w)
-        npt.assert_almost_equal(std_w, self._std_w)
+        self._fig_size = [13, 7]
 
-        mean_w_corr, std_w_corr = calc.weighted_avg_and_std(
-            self._x,
-            self._w,
-            corrected=True,
-        )
-        npt.assert_almost_equal(mean_w_corr, self._mean_w)
-        npt.assert_almost_equal(std_w_corr, self._std_w_corr)
+        self._x = [1, 1.5, 2, 2, 3, 5]
+        self._n_bin = 4
+        self._x_range = [1, 5]
+        self._img_path = 'test.png'
+        self._n_arr = np.array([2.0, 2.0, 1.0, 1.0])
+        self._bins = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
+
+    def tearDown(self):
+        """Unset test parameter values.
+
+        """
+        self._fig_size = None
+        self._x = None
+        self._n_bin = None
+        self._x_range = None
+
+        if os.path.exists(self._img_path):
+            os.remove(self._img_path)
+        self._img_path = None
+
+        self._n_arr = None
+        self._bins = None
+
+
+
+    def test_figure(self):
+        """Test ``cs_util.weighted_avg_and_std`` method.
+
+        """
+        fig = plots.figure(figsize=(self._fig_size[0], self._fig_size[1]))
+
+        # Check for return value
+        self.assertIsNotNone(fig, msg='Incorrect return type')
+
+        # Check image size
+        size = fig.get_size_inches()
+        for idx in (0, 1):
+            npt.assert_almost_equal(size[idx], self._fig_size[idx])
+
+    def test_plot_histograms(self):
+        """Test ``cs_util.plot_histograms`` method.
+
+        """
+        for vline_x, vline_lab in ((None, 1.2), (None, 'vlab')):
+            n_arr, bins = plots.plot_histograms(
+                [self._x],
+                ['hist 1'],
+                'title',
+                '$x$',
+                'freq',
+                self._x_range,
+                self._n_bin,
+                self._img_path,
+                density=False,
+                vline_x=vline_x,
+                vline_lab=vline_lab,
+            )
+
+            # Check return histogram data
+            npt.assert_almost_equal(n_arr[0], self._n_arr)
+            npt.assert_almost_equal(bins[0], self._bins)
+
+            # Check output plot file
+            self.assertTrue(os.path.exists(self._img_path))
