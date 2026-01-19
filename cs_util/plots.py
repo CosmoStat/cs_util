07765917dca682dc0ef69b21200ce90616452c7d
martinkilbinger <martin.kilbinger@cea.fr>
2026-01-19 15:55:24 +0000

    fixed sky plot

--- cs_util/plots.py
+++ cs_util/plots.py
@@ -449,9 +449,9 @@ class FootprintPlotter:
 
     # Dictionary storing region parameters
     _regions = {
-        "NGC": {"ra_0": 180, "extend": [120, 270, 20, 70], "vmax": 60},
-        "SGC": {"ra_0": 15, "extend": [-20, 45, 20, 45], "vmax": 60},
-        "fullsky": {"ra_0": 150, "extend": [0, 360, -90, 90], "vmax": 60},
+        "NGC": {"ra_0": 180, "extend": [120, 270, 20, 70], "vmin": 0, "vmax": 60},
+        "SGC": {"ra_0": 15, "extend": [-20, 45, 20, 45], "vmin": 0, "vmax": 60},
+        "fullsky": {"ra_0": 150, "extend": [0, 360, -90, 90], "vmin": 0, "vmax": 60},
     }
 
     def __init__(self, nside_coverage=32, nside_map=2048):
@@ -507,6 +507,7 @@ class FootprintPlotter:
         hsp_map,
         ra_0=0,
         extend=[120, 270, 29, 70],
+        vmin=0,
         vmax=60,
         projection=None,
         outpath=None,
@@ -527,6 +528,8 @@ class FootprintPlotter:
         extend : list, optional
             sky region, extend=[ra_low, ra_high, dec_low, dec_high];
             default is [120, 270, 29, 70]
+        vmin : float, optional
+            minimum pixel value to plot with color; default is 0
         vmax : float, optional
             maximum pixel value to plot with color; default is 60
         projection : skyproj.McBrydeSkyproj
@@ -560,7 +563,7 @@ class FootprintPlotter:
 
             # Create new projection
             projection = skyproj.McBrydeSkyproj(
-                ax=ax, lon_0=ra_0, extent=extend, autorescale=True, vmax=vmax
+                ax=ax, lon_0=ra_0, extent=extend, autorescale=False
             )
         else:
             ax = None
@@ -568,7 +571,7 @@ class FootprintPlotter:
         im = None
         try:
             im, lon_raster, lat_raster, values_raster = projection.draw_hspmap(
-                hsp_map, lon_range=extend[0:2], lat_range=extend[2:]
+                hsp_map, lon_range=extend[0:2], lat_range=extend[2:], vmin=vmin, vmax=vmax
             )
         except ValueError:
             msg = "No object found in region to draw"
@@ -577,6 +580,10 @@ class FootprintPlotter:
         projection.draw_milky_way(
             width=25, linewidth=1.5, color="black", linestyle="-"
         )
+
+        # Use skyproj's own methods to enforce extent
+        projection.set_autorescale(False)
+        projection.set_extent(extend)
 
         # Set axis labels
         if ax:
@@ -599,6 +606,10 @@ class FootprintPlotter:
 
         if title:
             plt.title(title, pad=5)
+
+        # Force extent again after all plotting operations to ensure it's respected
+        projection.set_autorescale(False)
+        projection.set_extent(extend)
 
         if outpath:
             plt.savefig(outpath)
@@ -624,7 +635,7 @@ class FootprintPlotter:
         hsp_map : hsp_HealSparseMap
             input map
         region : dict
-            region dictionary with keys 'ra_0', 'extend', 'vmax'
+            region dictionary with keys 'ra_0', 'extend', 'vmin', 'vmax'
         projection : skyproj.McBrydeSkyproj, optional
             if ``None`` (default), a new plot is created
         outpath : str, optional
@@ -648,6 +659,7 @@ class FootprintPlotter:
             hsp_map,
             region["ra_0"],
             region["extend"],
+            region["vmin"],
             region["vmax"],
             projection=projection,
             outpath=outpath,
